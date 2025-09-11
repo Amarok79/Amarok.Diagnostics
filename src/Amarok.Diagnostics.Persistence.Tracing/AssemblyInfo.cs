f789cdda30a3d618f0bfe26300896504b65c4a03
Olaf Kober <olaf.kober@outlook.com>
2025-09-11 13:52:32 +0200

    Sync solution/project setup with other repos

---
+++ src/Amarok.Diagnostics.Persistence.Tracing/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) 2024, Olaf Kober <olaf.kober@outlook.com>
+
+using System.Runtime.CompilerServices;
+
+
+// Friend Test Assemblies
+[assembly:
+    InternalsVisibleTo(
+        "Amarok.Diagnostics.Persistence.Tracing.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100691420b75c6bd08d4cde644c991ae9f5d0739ac5dc6dd6a7428fbfe03c292dae8cc9eece727e62fbbe59ef392c61ecafe563b96484e28def27eae70aff042cecaa2cfff56fed30fe6fbed507b8f159f4a43608f284a9186b8aeffd25ce944dcae0c14f61e00fe997cdf28dfa0824a65ab491aa7caddd233e6faceecadfc80eb3"
+    )]
